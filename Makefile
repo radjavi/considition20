e9645555d7d85c63a58beb04185b49001ab22504
Iman Radjavi <radjavi@hotmail.com>
2020-09-27 13:31:13 +0200

    minor modifications and Makefile

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: t1 t2
+
+t1:
+	python main.py training1
+
+t2:
+	python main.py training2
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import api
 from game_layer import GameLayer
+import sys
 
-api_key = ""   # TODO: Your api key here
+api_key = "af79a1fd-d071-4109-8776-a31b815057ad"
 # The different map names can be found on considition.com/rules
-map_name = "training1"  # TODO: You map choice here. If left empty, the map "training1" will be selected.
+# Map name taken as command line argument. 
+# If left empty, the map "training1" will be selected.
+map_name = sys.argv[1] if len(sys.argv) > 1 else "training1"
 
-game_layer: GameLayer = None
+game_layer: GameLayer = GameLayer(api_key)
 
 
 def main():
-    game_layer.new_game(map_name)
-    print("Starting game: " + game_layer.game_state.game_id)
-    game_layer.start_game()
-    while game_layer.game_state.turn < game_layer.game_state.max_turns:
-        take_turn()
-    print("Done with game: " + game_layer.game_state.game_id)
-    print("Final score was: " + str(game_layer.get_score()["finalScore"]))
+    try:
+        game_layer.new_game(map_name)
+        print("Starting game: " + game_layer.game_state.game_id)
+        print("Map:", map_name)
+        game_layer.start_game()
+        while game_layer.game_state.turn < game_layer.game_state.max_turns:
+            take_turn()
+        print("Done with game: " + game_layer.game_state.game_id)
+        print("Final score was: " + str(game_layer.get_score()["finalScore"]))
+    except: # End game session in case of exceptions
+        print("\nEnding game: ", game_layer.game_state.game_id)
+        game_layer.end_game()
 
 
 def take_turn():
