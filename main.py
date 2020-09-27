04d7ce415832d3a03edf7017036b8e04a84a6ffd
Belmin Dervisevic <belmin.der@gmail.com>
2020-09-27 21:23:28 +0200

    Implement logic for upgrades

--- main.py
+++ main.py
@@ -14,29 +14,29 @@ API_KEY = os.getenv("API_KEY")
 # If left empty, the map "training1" will be selected.
 map_name = sys.argv[1] if len(sys.argv) > 1 else "training1"
 
-game_layer: GameLayer = GameLayer(API_KEY)
+GAME_LAYER: GameLayer = GameLayer(API_KEY)
 
 
 def main():
     try:
-        game_layer.new_game(map_name)
-        print("Starting game: " + game_layer.game_state.game_id)
+        GAME_LAYER.new_game(map_name)
+        print("Starting game: " + GAME_LAYER.game_state.game_id)
         print("Map:", map_name)
-        game_layer.start_game()
-        while game_layer.game_state.turn < game_layer.game_state.max_turns:
+        GAME_LAYER.start_game()
+        while GAME_LAYER.game_state.turn < GAME_LAYER.game_state.max_turns:
             take_turn()
-        print("Done with game: " + game_layer.game_state.game_id)
-        print("Final score was: " + str(game_layer.get_score()["finalScore"]))
+        print("Done with game: " + GAME_LAYER.game_state.game_id)
+        print("Final score was: " + str(GAME_LAYER.get_score()["finalScore"]))
     except KeyboardInterrupt:  # End game session in case of exceptions
-        print(f"\nForce quit game: {game_layer.game_state.game_id}")
-        game_layer.end_game()
+        print(f"\nForce quit game: {GAME_LAYER.game_state.game_id}")
+        GAME_LAYER.end_game()
     except Exception as e:  # Catching generic exceptions
         print(f"Error: {e}")
-        game_layer.end_game()
+        GAME_LAYER.end_game()
 
 
 def take_turn():
-    state = game_layer.game_state
+    state = GAME_LAYER.game_state
 
     # Take one of the following actions in order of priority #
     if residence_maintenance(state):
@@ -45,15 +45,17 @@ def take_turn():
         pass
     elif build_residence(state):
         pass
+    elif available_upgrades(state):
+        pass
     elif place_residence(state):
         pass
     else:
-        game_layer.wait()
+        GAME_LAYER.wait()
     # --- #
 
-    for message in game_layer.game_state.messages:
+    for message in GAME_LAYER.game_state.messages:
         print(message)
-    for error in game_layer.game_state.errors:
+    for error in GAME_LAYER.game_state.errors:
         print("Error: " + error)
 
 
@@ -61,7 +63,7 @@ def take_turn():
 def residence_maintenance(state):
     for residence in state.residences:
         if residence.health < 50:
-            game_layer.maintenance((residence.X, residence.Y))
+            GAME_LAYER.maintenance((residence.X, residence.Y))
             return True
 
 
@@ -69,7 +71,7 @@ def residence_maintenance(state):
 def regulate_temperature(state):
     for residence in state.residences:
         if residence.temperature < 18 or residence.temperature > 24:
-            blueprint = game_layer.get_residence_blueprint(residence.building_name)
+            blueprint = GAME_LAYER.get_residence_blueprint(residence.building_name)
             energy = (
                 blueprint.base_energy_need
                 + (21 - residence.temperature)
@@ -78,7 +80,7 @@ def regulate_temperature(state):
                 / 1
                 - residence.current_pop * 0.04
             )
-            game_layer.adjust_energy_level((residence.X, residence.Y), energy)
+            GAME_LAYER.adjust_energy_level((residence.X, residence.Y), energy)
             return True
 
 
@@ -86,23 +88,45 @@ def regulate_temperature(state):
 def build_residence(state):
     for residence in state.residences:
         if residence.build_progress < 100:
-            game_layer.build((residence.X, residence.Y))
+            GAME_LAYER.build((residence.X, residence.Y))
             return True
 
 
-# Place a new (cheapest) residence at an available spot
+# Place a new residence at an available spot
 def place_residence(state):
-    cheapest_residence = min(state.available_residence_buildings, key=lambda x: x.cost)
-    if state.housing_queue >= cheapest_residence.max_pop:
-        for i in range(len(state.map)):
-            for j in range(len(state.map)):
-                if state.map[i][j] == 0:
-                    x = i
-                    y = j
-                    break
-        state.map[x][y] = 1
-        game_layer.place_foundation((x, y), cheapest_residence.building_name)
-        return True
+    residence = _choose_residence(state)
+    # if state.housing_queue >= residence.max_pop:
+    for i in range(len(state.map)):
+        for j in range(len(state.map)):
+            if state.map[i][j] == 0:
+                x = i
+                y = j
+                break
+    state.map[x][y] = 1
+    GAME_LAYER.place_foundation((x, y), residence.building_name)
+    return True
+
+
+def available_upgrades(state):
+    upgrade = _choose_upgrade(state)
+    for residence in state.residences:
+        if upgrade.name not in residence.effects:
+            GAME_LAYER.buy_upgrade(
+                (residence.X, residence.Y),
+                upgrade.name,
+            )
+            return True
+
+
+def _choose_upgrade(state):
+    return max(state.available_upgrades, key=lambda x: x.cost)
+
+
+def _choose_residence(state):
+    return max(state.available_residence_buildings, key=lambda x: x.cost)
+    # return min(state.available_residence_buildings, key=lambda x: x.cost)
+    # import random
+    # return random.choice(state.available_residence_buildings)
 
 
 if __name__ == "__main__":
